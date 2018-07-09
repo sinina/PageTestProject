e56693f4297d027b5d7f2cfc8a00492796cfecb6
YoonSangWon <32891939+YoonSangWon@users.noreply.github.com>
2018-07-09 17:26:00 +0900

    Update MemberChartService.java

--- PageTestProject/src/com/matajo/pitpet/admin/model/service/MemberChartService.java
+++ PageTestProject/src/com/matajo/pitpet/admin/model/service/MemberChartService.java
@@ -6,18 +6,33 @@ import com.matajo.pitpet.admin.model.dao.MemberChartDao;
 import com.matajo.pitpet.common.JDBCTemplate;
 
 public class MemberChartService {
-	 public int getTotalCount(String area) {
+	
+    public int getOMemberCount(String area) {
 
-	        Connection con = JDBCTemplate.getConnection();
-	        int result = new MemberChartDao().getTotalMemberCount(con,area);
+        Connection con = JDBCTemplate.getConnection();
+        int result = new memberChartDao().getOwnerCount(con, area);
 
-	        if (0 < result) {
-	            JDBCTemplate.commit(con);
-	        } else {
-	            JDBCTemplate.rollback(con);
-	        }
-	        JDBCTemplate.close(con);
-	        return result;
-	    }
+        if (0 < result) {
+            JDBCTemplate.commit(con);
+        } else {
+            JDBCTemplate.rollback(con);
+        }
+        JDBCTemplate.close(con);
+        return result;
+    }
+
+    public int getPSMemberCount(String area) {
+
+        Connection con = JDBCTemplate.getConnection();
+        int result = new memberChartDao().getPetsCount(con, area);
+
+        if (0 < result) {
+            JDBCTemplate.commit(con);
+        } else {
+            JDBCTemplate.rollback(con);
+        }
+        JDBCTemplate.close(con);
+        return result;
+    }
 
 }
