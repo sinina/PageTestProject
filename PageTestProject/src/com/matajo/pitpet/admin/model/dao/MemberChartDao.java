b73f6b47e5fe1253115523d66b8e0a8b7c772887
YoonSangWon <32891939+YoonSangWon@users.noreply.github.com>
2018-07-09 17:25:26 +0900

    Update MemberChartDao.java

--- PageTestProject/src/com/matajo/pitpet/admin/model/dao/MemberChartDao.java
+++ PageTestProject/src/com/matajo/pitpet/admin/model/dao/MemberChartDao.java
@@ -8,32 +8,62 @@ import java.sql.Statement;
 import com.matajo.pitpet.common.JDBCTemplate;
 
 public class MemberChartDao {
-	 public int getTotalMemberCount(Connection con, String area){
-	        Statement stmt = null;
-	        ResultSet rs = null;
-	        int result =0;
+	  public int getOwnerCount(Connection con, String area){
+            //총방문자수 가져오는 메소드
+            Statement stmt = null;
+            ResultSet rs = null;
+            int result =0;
 
-	        String query ="";
+            String query = "";
 
-	        try {
-	            stmt= con.createStatement();
-	            query="SELECT COUNT(*) AS TotalCnt" +
-	                    "  FROM MEMBER " +
-	                    "WHERE M_MEMBER_CODE = '2' AND M_ADDRESS LIKE '%"+area+"%'";
-	            rs= stmt.executeQuery(query);
+            try {
+                stmt= con.createStatement();
+                query="select count(*)" +
+                        "  from MEMBER" +
+                        "where M_MEMBER_CODE = '1' and M_ADDRESS like '%"+area+"%'";
+                rs= stmt.executeQuery(query);
 
-	            if(rs.next()){
-	                result = rs.getInt("TotalCnt");
-	            }
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        }finally{
-	            JDBCTemplate.close(rs);
-	            JDBCTemplate.close(stmt);
-	        }
-	        return result;
+                if(rs.next()){
+                    result = rs.getInt("TotalOwnerCnt");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }finally{
+            JDBCTemplate.close(rs);
+            JDBCTemplate.close(stmt);
+        }
+        return result;
+
+    }
+
+    public int getPetsCount(Connection con, String area){
+        //총방문자수 가져오는 메소드
+        Statement stmt = null;
+        ResultSet rs = null;
+        int result =0;
+
+        String query = "";
+
+        try {
+            stmt= con.createStatement();
+            query="select count(*)" +
+                    "  from MEMBER" +
+                    "where M_MEMBER_CODE = '2' and M_ADDRESS like '%"+area+"%'";
+            rs= stmt.executeQuery(query);
+
+            if(rs.next()){
+                result = rs.getInt("TotalPetsCnt");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            JDBCTemplate.close(rs);
+            JDBCTemplate.close(stmt);
+        }
+        return result;
+
+    }
 
 
-	    }
 
 }
